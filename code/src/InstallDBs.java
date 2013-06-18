37c708bba53ec3f1ab4503043088fc7c90a0110c
andreroehrig <andre.roehrig@gmail.com>
2013-06-18 18:29:26 +0200

    import dd.sql on arch

--- code/src/InstallDBs.java
+++ code/src/InstallDBs.java
@@ -1,4 +1,6 @@
 import java.sql.DatabaseMetaData;
+import java.util.List;
+import java.io.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,8 +10,33 @@ import java.sql.DatabaseMetaData;
  * To change this template use File | Settings | File Templates.
  */
 public class InstallDBs {
-    public void exec(SQLConnection connection)     {
-    	List<String> connection.getExistingTables();
-    	for 
+    public void exec(SQLConnection connection,
+        String path,
+        String serverURL,
+        String serverPort,
+        String databaseName,
+        String userName,
+        String password
+    )
+    {
+    	List<String> tablelist = connection.getExistingTables();
+    	if (tablelist.contains("")){
+            try {
+                String line;
+                Process p = Runtime.getRuntime().exec
+                        ("psql -U "+userName+"-d "+databaseName+" -h "+serverURL+" -f "+path+"dd.sql");
+                BufferedReader input =
+                        new BufferedReader
+                                (new InputStreamReader(p.getInputStream()));
+                while ((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+                input.close();
+            }
+            catch (Exception err) {
+                err.printStackTrace();
+            }
+
+        }
     }
 }
