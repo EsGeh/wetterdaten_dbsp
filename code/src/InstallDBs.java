4c7780260f560ba3633bbb7d4b54115c92217b60
andreroehrig <andre.roehrig@gmail.com>
2013-06-18 16:22:11 +0200

    start installdb

---
+++ code/src/InstallDBs.java
@@ -0,0 +1,14 @@
+import java.sql.DatabaseMetaData;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andre
+ * Date: 18.06.13
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class InstallDBs {
+    public void exec(SQLConnection connection)     {
+        DatabaseMetaData meta =
+    }
+}
